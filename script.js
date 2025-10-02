136a5a34b3b18c1a596f82631ba693a38a24eba2
LosFireFX <118818699+LosFireFX@users.noreply.github.com>
2025-10-02 15:53:04 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function pushDiscord(){
+    if(window.confirm("Voulez vous rejoindre la communauté ?")){
+        window.open("https://discord.gg/rffRrPDucB")
+    }
+}
+
+function pushDonate(){
+    if(window.confirm("Voulez vous faire un don ?")){
+        window.open("https://ko-fi.com/picalosse")
+    }
+}
